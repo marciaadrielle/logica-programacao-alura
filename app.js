c407de6c44a7ea8a1b2587574b20a14577e26e84
Márcia Araújo <marcia.adrielle@gmail.com>
2025-08-03 13:03:15 -0300

    feat: jogo do número secreto versão 2.0

--- app.js
+++ app.js
@@ -1,33 +1,74 @@
-alert('Boas vindas ao jogo do número secreto');
-//let secretNumber = parseInt(Math.random() * 100 + 1);
+let drawnNumbers = [];
 let maximumNumber = 100;
-let secretNumber= parseInt(Math.random() * maximumNumber + 1);
-let guess;
-let attempt = 1;
+let secretNumber = randomNumber();
+let attempts = 1;
 
 
-// Enquanto o chute não foi igual ao número secreto
-while (guess != secretNumber) {
-    guess = parseInt(prompt(`Escolha um número entre 1 e ${maximumNumber}`));
-    // Se o chute for igual ao número secreto    
-    if (guess == secretNumber) {
-        break;
+function showTextOnTheScreen(tag, text) {
+    let field = document.querySelector(tag);
+    field.innerHTML = text;
+    //responsiveVoice.speak(text,'Brazilian Portuguese Female', {rate: 1.2});
+    if ('speechSynthesis' in window){
+        let utterance = new SpeechSynthesisUtterance(text);
+        utterance.lang = 'pt-BR';
+        utterance.rate = 1.2;
+        window.speechSynthesis.speak(utterance);
     } else {
-        if (guess > secretNumber) {
-            alert(`O número secreto é menor que ${guess}.`);
-        } else {
-            alert(`O número secreto é maior que ${guess}.`);
-        }
-       attempt++
+        console.log("Web Speech API não suportada neste navegador.");
     }
+
 }
 
-let wordAttempt = attempt > 1 ? 'tentativas' : 'tentativa';
-alert(`Isso ai! Você descobriu o número secreto ${secretNumber} com ${attempt} ${wordAttempt}.`);
+function showInitialMessage() {
+    showTextOnTheScreen('h1', 'Jogo do número secreto');
+    showTextOnTheScreen('p', `Escolha um número de 1 a ${maximumNumber}`);
 
-/* if (gues > 1){
-alert(`Isso ai! Você descobriu o número secreto ${secretNumber} com ${attempt} tentativa.`);
-} else {
-    alert(`Isso ai! Você descobriu o número secreto ${secretNumber} com ${attempt} tentativas.`);
-} */
 
+}
+showInitialMessage();
+
+function checkGuess() {
+    let guess = parseInt(document.querySelector('input').value);
+
+    if (guess == secretNumber) {
+        showTextOnTheScreen('h1', 'Acertou!');
+        let wordAttempts = attempts > 1 ? 'tentativas' : 'tentativa';
+        let messageAttempts = `Você descobriu o número secreto com ${attempts} ${wordAttempts}!`;
+        showTextOnTheScreen('p', messageAttempts);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (guess > secretNumber) {
+           showTextOnTheScreen('p', 'O número secreto é menor.');
+        } else {
+            showTextOnTheScreen('p', 'O número secreto é maior.');
+        }
+        attempts++;
+        clearInput();
+    }
+}
+function randomNumber() {
+    let chosenNumber = parseInt(Math.random() * maximumNumber + 1);
+    let numbersOfElementsDrawn = drawnNumbers.length;
+
+    if (numbersOfElementsDrawn == maximumNumber){
+        drawnNumbers = [];
+    }
+    if (drawnNumbers.includes(chosenNumber)){
+        return randomNumber();
+    } else {
+        drawnNumbers.push(chosenNumber);
+        console.log(drawnNumbers);
+        return chosenNumber;
+    }
+}
+function clearInput() {
+    guess = document.querySelector('input');
+    guess.value = ' ';
+}
+function restartGame() {
+    secretNumber = randomNumber();
+    clearInput();
+    attempts = 1;
+    showInitialMessage();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
